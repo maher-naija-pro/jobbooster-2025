460c3ad0b14993cccec46f3a29e2474e8d17d895
maher.naija <maher.naija@gmail.com>
2025-10-04 16:15:39 +0000

    update

--- src/app/layout/Navbar.tsx
+++ src/app/layout/Navbar.tsx
@@ -24,9 +24,9 @@ const Navbar = () => {
 
   return (
     <header className="bg-white border-b border-gray-200 shadow-sm sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
-          <div className="flex items-center">
+      <div className="  px-4 sm:px-6 lg:px-8">  
+        <div className="flex justify-between items-center ">  
+          <div className=""> 
             <Link
               href="/"
               onClick={handleLogoClick}
